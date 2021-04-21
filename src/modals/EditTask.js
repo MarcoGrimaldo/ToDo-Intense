db3d7979a24458f81f5f5731c6bdd8c0ea100066
Marco Grimaldo <marco.a.grimaldo98@gmil.com>
2021-04-21 14:47:18 -0500

    fixing dependencies

--- src/modals/EditTask.js
+++ src/modals/EditTask.js
@@ -21,7 +21,7 @@ const EditTaskPopup = ({modal, toggle, updateTask, taskObj}) => {
     useEffect(() => {
         setTaskName(taskObj.Name)
         setDescription(taskObj.Description)
-    },[])
+    },[taskObj])
 
     const handleUpdate = (e) => {
         e.preventDefault();
